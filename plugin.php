928d1a6332fc9547a0a45a9bc85832fdf42b70c6
Camila L. Oliveira (Mi) <ola@camilaloliveira.com>
2026-05-09 20:15:43 -0300

    Fix: atualizando plugin para comportar o nome do site

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Rodapé Automático
  * Description: Adiciona um shortcode para exibir o copyright com o ano atual.
- * Version: 1.0
+ * Version: 1.1
  * Author: Camila Leite
  * Author URL: https://go.camilaloliveira.com/dev
  * Text Domain: footer-copyright
@@ -14,8 +14,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function custom_copyright_shortcode() {
+    // Captura o ano atual do servidor
     $ano_atual = date("Y");
-    $texto = "&copy; " . $ano_atual . ". Camila L. Oliveira. Todos os direitos reservados.";
+
+    // Captura o nome do site definido no painel do WordPress
+    $nome_do_site = get_bloginfo('name');
+
+    // Monta a estrutura com variáveis
+    $texto = "&copy; " . $ano_atual . ". " . $nome_do_site . ". Todos os direitos reservados.";
     
     return '<span class="custom-footer-copyright">' . $texto . '</span>';
 }
